2ddd0e3e16be11529da004d0686ce5c4728af834
macxq <t.maciek@gmail.com>
2021-09-10 11:38:40 +0200

    hello world example

---
+++ __init__.py
@@ -0,0 +1,24 @@
+"""
+The "hello world" custom component.
+This component implements the bare minimum that a component should implement.
+Configuration:
+To use the hello_world component you will need to add the following to your
+configuration.yaml file.
+hello_world:
+"""
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+# The domain of your component. Should be equal to the name of your component.
+DOMAIN = "foxess"
+
+
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up a skeleton component."""
+    # States are in the format DOMAIN.OBJECT_ID.
+    hass.states.set('foxess.Hello_World', 'Works!')
+
+    # Return boolean to indicate that initialization was successfully.
+    return True
